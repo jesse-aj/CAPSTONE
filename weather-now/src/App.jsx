422d9c8d4e0d08fd7432d86a6b1f50c10a585929
jesse-aj <appiahjesse123@gmail.com>
2025-10-14 23:53:17 +0000

    created a form to handle search and display results when users type in

--- weather-now/src/App.jsx
+++ weather-now/src/App.jsx
@@ -1,7 +1,47 @@
 import { useState } from "react";
 import { fetchWeather } from "./services/WeatherService";
-
+ 
+export default function App() {
 const [city , setCity ] = ("");
 const [weather, setWeather ] = (null);
 
-export default fetchWeather 
+// This will handle the search when a user searches for a city
+const handleSearch = async () => {
+  try 
+  {
+
+    const data = await fetchWeather(city);
+    setWeather(data);
+  } catch {
+    alert("City not found")
+  }
+};
+
+return (
+  <div>
+    <h1>Weather Dashboard</h1>
+  //This is where the actual form logic is placed
+  <input
+     type="text"
+     placeholder="Enter City"
+     value={city}
+     // This triggers whenever the user types
+     onChange = { (e) => setCity(e.target.value) } />// this gives the app the typed text and it is saved in setCity
+    
+
+     <button onClick={handleSearch}> Search</button>
+       
+      // Now we show the results
+      { weather && (
+        <div>
+        <h2>{weather.name}</h2>
+        <h2>{weather.weather[0].description}</h2>
+        <p>{weather.main.temp}°C</p>
+        </div>
+      ) }
+
+  </div>
+);
+}
+
+
