ce9b5409fc171e54139769c02d698b34e2716547
jesse-aj <appiahjesse123@gmail.com>
2025-10-18 10:58:00 +0000

    Configured my tailwindcss and styled my landing page

--- weather-now/tailwind.config.js
+++ weather-now/tailwind.config.js
@@ -2,8 +2,23 @@
 export default {
   content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: 'class',
-  theme: {
-    extend: {},
+   theme: {
+    extend: {
+ colors: {
+        primary: "#359EFF",
+        "background-light": "#f5f7f8",
+        "background-dark": "#0f1923",
+      },
+      fontFamily: {
+        display: ["Space Grotesk", "sans-serif"],
+      },
+      borderRadius: {
+        DEFAULT: "0.5rem",
+        lg: "1rem",
+        xl: "1.5rem",
+        full: "9999px",
+      },
+    },
   },
-  plugins: [],
+  plugins: [require("@tailwindcss/forms"), require("@tailwindcss/container-queries")],
 };
