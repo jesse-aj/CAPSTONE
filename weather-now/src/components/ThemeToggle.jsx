64fad6ce2fc52ba96594bfa9650184a58465ef92
jesse-aj <appiahjesse123@gmail.com>
2026-01-09 01:21:42 +0000

    Refrsctored the designs for Theme Toggle

--- weather-now/src/components/ThemeToggle.jsx
+++ weather-now/src/components/ThemeToggle.jsx
@@ -1,40 +1,26 @@
-import { useEffect, useState } from "react";
-  // this is a function that handles dark themes
 const ThemeToggle = () => {
-    const [darkMode, setDarkMode] = useState(
-        localStorage.getItem("theme") === "dark"
-    );
+  const [darkMode, setDarkMode] = useState(
+    localStorage.getItem("theme") === "dark"
+  );
 
-
-//THis allows it to show on app starts
-useEffect( () => {
-    if(darkMode) {
-        document.documentElement.classList.add("dark") ;
-        localStorage.setItem("theme", "dark");
+  useEffect(() => {
+    if (darkMode) {
+      document.documentElement.classList.add("dark");
+      localStorage.setItem("theme", "dark");
     } else {
-        document.documentElement.classList.remove("dark");
-        localStorage.setItem("theme", "light")
+      document.documentElement.classList.remove("dark");
+      localStorage.setItem("theme", "light");
     }
+  }, [darkMode]);
 
-}, [darkMode]
-
-);
-
-
-return (
-    <div className="flex justify-end p-4">
-    <button 
-    onClick={()=> setDarkMode(!darkMode)}
-    className="p-2 rounded-2xl bg-gray-300 dark:bg-gray-700 text-black dark:text-white">
-        {darkMode ? " Light Mode" : " Dark Mode"}
-
+  return (
+    <button
+      onClick={() => setDarkMode(!darkMode)}
+      className="flex h-10 w-10 items-center justify-center rounded-lg bg-[#359EFF]/10 text-[#359EFF] dark:bg-[#359EFF]/20 hover:bg-[#359EFF]/20 dark:hover:bg-[#359EFF]/30 transition-colors"
+    >
+      <span className="material-symbols-outlined">
+        {darkMode ? "light_mode" : "dark_mode"}
+      </span>
     </button>
-  </div>
-
-
-);
-
+  );
 };
-
-
-export default ThemeToggle;
